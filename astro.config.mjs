6f8e905ccb2223fe1a321ae0096aaa15b5575878
laurapamelaarandamedrano-dot <lapaarmeine@gmail.com>
2026-07-26 23:57:40 -0700

    feat: portal inicial SDHP

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://laurapamelaarandamedrano.github.io',
+  base: '/portal-sdhp',
+  output: 'static',
+});
